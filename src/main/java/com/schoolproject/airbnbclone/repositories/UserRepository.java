bbf7617d0479b66787d9285dc44305439b663d3b
Theodoros Theocharis <thothe7@gmail.com>
2023-09-14 19:57:51 +0300

    Get all users now doesn't return the admin

--- src/main/java/com/schoolproject/airbnbclone/repositories/UserRepository.java
+++ src/main/java/com/schoolproject/airbnbclone/repositories/UserRepository.java
@@ -1,13 +1,23 @@
 package com.schoolproject.airbnbclone.repositories;
 
 import com.schoolproject.airbnbclone.models.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer>, PagingAndSortingRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
+
+    @NonNull
+    @Transactional
+    @Query("SELECT u From User u WHERE u.username != 'admin'")
+    Page<User> findAll(@NonNull Pageable pageable);
 
 }
