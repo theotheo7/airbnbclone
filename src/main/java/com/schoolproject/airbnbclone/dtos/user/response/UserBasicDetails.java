b2b7dd1d49dc7b13a2a95380a013e42bc10f1e43
Theodoros Theocharis <thothe7@gmail.com>
2023-09-14 19:57:15 +0300

    User id generation tweak and basic details dto commit

---
+++ src/main/java/com/schoolproject/airbnbclone/dtos/user/response/UserBasicDetails.java
@@ -0,0 +1,25 @@
+package com.schoolproject.airbnbclone.dtos.user.response;
+
+import com.schoolproject.airbnbclone.models.User;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UserBasicDetails implements Serializable {
+
+    private final String firstName;
+    private final String lastName;
+    private final String phoneNumber;
+    private final String username;
+
+    public UserBasicDetails(User user) {
+
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.phoneNumber = user.getPhoneNumber();
+        this.username = user.getUsername();
+
+    }
+
+}
