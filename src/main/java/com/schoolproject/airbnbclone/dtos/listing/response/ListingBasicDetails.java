b1e60bac0efb35cf530d5b40050c44067ac22b92
Theodoros Theocharis <thothe7@gmail.com>
2023-09-30 11:09:36 +0300

    Update listing basic details

--- src/main/java/com/schoolproject/airbnbclone/dtos/listing/response/ListingBasicDetails.java
+++ src/main/java/com/schoolproject/airbnbclone/dtos/listing/response/ListingBasicDetails.java
@@ -4,20 +4,29 @@ import com.schoolproject.airbnbclone.models.Listing;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Data
 public class ListingBasicDetails implements Serializable {
 
+    private Long id;
     private String name;
     private String image;
+    private BigDecimal price;
+    private String type;
+    private Integer beds;
 
     public ListingBasicDetails(Listing listing) {
+        this.id = listing.getId();
         this.name = listing.getName();
         if (!listing.getImages().isEmpty()) {
             this.image = listing.getImages().iterator().next().getPath();
         } else {
             this.image = "";
         }
+        this.price = listing.getPrice();
+        this.type = listing.getType();
+        this.beds = listing.getBeds();
     }
 
 }
